040d5cc83afa629a90deda011883f68ba06900b1
mada.petru <mada.petru@digitalexplorer.ro>
2024-12-21 20:37:47 +0200

    wip to be responsive at least on my phone

--- src/app/shared/provider/table/field-operations-history-provider.ts
+++ src/app/shared/provider/table/field-operations-history-provider.ts
@@ -1,7 +1,7 @@
 import {TableOperationHistory} from "../../model/table/operations-history/table-operation-history";
 
 export class TableOperationsHistoryProvider {
-  private static _columns: string[] = ['Date', 'Estimated cost', 'Estimated harvest', 'Estimated revenue', 'Type of plant', 'Operation description', 'Operation column'];
+  private static _columns: string[] = ['Date', 'Cost', 'Harvest', 'Revenue', 'Plant', 'Operation', 'X'];
 
   static get columns(): string[] {
     return this._columns;
@@ -9,11 +9,10 @@ export class TableOperationsHistoryProvider {
 
   public static getAttributeBasedOnColumn(column: string, element: TableOperationHistory): any {
     if (column === 'Date') return element.date;
-    if (column === 'Estimated cost') return element.estimatedCost + ' ' + element.estimatedCostCurrencyType;
-    if (column === 'Estimated harvest') return element.estimatedHarvest + ' ' + element.estimatedHarvestMeasureType;
-    if (column === 'Operation description') return element.operationDescription;
-    if (column === 'Estimated profit') return element.operationDescription;
-    if (column === 'Type of plant') return element.typeOfPlant;
-    if (column === 'Estimated revenue') return element.estimatedRevenue + ' ' + element.estimatedRevenueCurrencyType;
+    if (column === 'Cost') return element.estimatedCost + ' ' + element.estimatedCostCurrencyType;
+    if (column === 'Harvest') return element.estimatedHarvest + ' ' + element.estimatedHarvestMeasureType;
+    if (column === 'Operation') return element.operationDescription;
+    if (column === 'Plant') return element.typeOfPlant;
+    if (column === 'Revenue') return element.estimatedRevenue + ' ' + element.estimatedRevenueCurrencyType;
   }
 }
