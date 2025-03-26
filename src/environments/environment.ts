5f73c7a92c1e50acce51a6b990deb841fab9c3a3
mada.petru <mada.petru@digitalexplorer.ro>
2025-03-26 15:51:53 +0200

    change prod url to the render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://agro-admin-be-free.bravepebble-5e78a0a3.germanywestcentral.azurecontainerapps.io',
+  apiUrl: 'https://agriculture-administration-app-be.onrender.com',
   redirectUrl: 'https://agro-admin-fe-free.bravepebble-5e78a0a3.germanywestcentral.azurecontainerapps.io/',
   keycloakUrl:'http://localhost:9090'
 };
